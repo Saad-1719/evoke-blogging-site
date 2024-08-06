fb6468507d6ead3f51862e61b079c4536f96ddca
Saad <115335100+Saad-1719@users.noreply.github.com>
2024-08-06 15:02:40 +0500

    chore: Add Blog and Newsletter components, and update layout with Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,11 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
-import Layout from "./home/LayoutWrapper";
 import Link from "next/link";
-// import { useRouter } from "next/router";
 
 const Navbar = () => {
 	const [isVisible, setIsVisible] = useState(true);
-	// const router = useRouter();
+	const [view, setView] = useState(false);
+
 	useEffect(() => {
 		let lastScrollTop = 0;
 		const handleScroll = () => {
@@ -25,27 +24,175 @@ const Navbar = () => {
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, []);
 
-	// const isActive = (path:string) => router.pathname === path ? 'text-black' : 'text-gray-400';
-
 	return (
-		<header className={`bg-slate-50 backdrop-blur-lg fixed flex items-center top-0 left-0 w-full z-10 transition-transform duration-300 ${isVisible ? 'translate-y-0' : '-translate-y-full'}`}>
-			{/* <Layout> */}
-				<nav className="flex justify-between items-center pt-4 pb-4 w-3/4 mx-auto">
-					<ul>
-						<li className="flex text-2xl font-bold text-Black  px-4 py-2 ">Evoke.</li>
+		<header
+			className={`bg-slate-50 backdrop-blur-lg fixed flex items-center top-0 left-0 w-full z-10 transition-transform duration-300 ${
+				isVisible ? "translate-y-0" : "-translate-y-full"
+			}`}
+		>
+			<nav className="flex justify-between items-center pt-4 pb-4 w-3/4 mx-auto">
+				<ul>
+					<li className="flex text-2xl font-bold text-black px-4 py-2">
+						Evoke.
+					</li>
+				</ul>
+
+				{/* hamburger */}
+				<ul>
+					<li
+						className="hover:cursor-pointer lg:hidden flex relative"
+						onClick={() => setView(!view)}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							className={`w-6 h-6 transition-transform duration-300 ${view ? "rotate-90" : "rotate-0"}`}
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+							/>
+						</svg>
+					</li>
+				</ul>
+
+				{/* pc screen menu */}
+				<ul className="lg:flex lg:justify-center lg:items-center space-x-6 hidden">
+					{["/", "/blog", "/pov", "/gallery", "/aboutme"].map((href, index) => (
+						<Link
+							key={index}
+							href={href}
+							className="rounded-full px-4 py-2 text-gray-400 hover:text-black transition-colors duration-75 ease-in-out font-semibold hover:cursor-pointer underline-animation"
+						>
+							{["Home", "Blogs", "POV", "Gallery", "About Me"][index]}
+						</Link>
+					))}
+				</ul>
+			</nav>
+
+			{/* mobile and tab view menu */}
+			{view && (
+				<div className={`flex flex-col absolute top-20 right-0 w-full p-4 z-10 bg-slate-50 lg:hidden transition-transform duration-300 ease-in-out ${view ? "scale-100" : "scale-0"}`}>
+					<ul className="flex flex-col items-center space-y-2 w-3/4 sm:w-1/4 mx-auto text-center">
+						{["/", "/blog", "/pov", "/gallery", "/aboutme"].map((href, index) => (
+							<Link
+								key={index}
+								href={href}
+								className="rounded-lg bg-black w-full py-2 text-white/90"
+							>
+								{["Home", "Blogs", "POV", "Gallery", "About Me"][index]}
+							</Link>
+						))}
 					</ul>
-					<ul className="flex justify-center items-center space-x-6">
-						<Link href="/" className="rounded-full px-4 py-2 text-gray-400 hover:text-black transition-colors duration-75 ease-in-out font-semibold hover:cursor-pointer underline-animation ">Home</Link>
-						<Link href="/blog" className="rounded-full px-4 py-2 text-gray-400 hover:text-black transition-colors duration-75 ease-in-out font-semibold hover:cursor-pointer underline-animation ">Blogs</Link>
-						<Link href="/pov" className="rounded-full px-4 py-2 text-gray-400 hover:text-black transition-colors duration-75 ease-in-out font-semibold hover:cursor-pointer underline-animation ">POV</Link>
-						<Link href="/gallery" className="rounded-full px-4 py-2 text-gray-400 hover:text-black transition-colors duration-75 ease-in-out font-semibold hover:cursor-pointer underline-animation ">Gallery</Link>
-						<Link href="/aboutme" className="rounded-full px-4 py-2 text-gray-400 hover:text-black transition-colors duration-75 ease-in-out font-semibold hover:cursor-pointer underline-animation ">About Me</Link>
-					</ul>
-				</nav>
-			{/* </Layout> */}
+				</div>
+			)}
 		</header>
 	);
 };
 
 export default Navbar;
 
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import Link from "next/link";
+// import 'aos/dist/aos.css';
+
+// const Navbar = () => {
+// 	const [isVisible, setIsVisible] = useState(true);
+// 	const [view, setView] = useState(false);
+
+// 	useEffect(() => {
+// 		let lastScrollTop = 0;
+// 		const handleScroll = () => {
+// 			let currentScrollTop = window.scrollY;
+// 			if (currentScrollTop > lastScrollTop) {
+// 				setIsVisible(false);
+// 			} else {
+// 				setIsVisible(true);
+// 			}
+// 			lastScrollTop = currentScrollTop;
+// 		};
+
+// 		window.addEventListener("scroll", handleScroll);
+// 		return () => window.removeEventListener("scroll", handleScroll);
+// 	}, []);
+
+// 	return (
+// 		<header
+// 			className={`bg-slate-50 backdrop-blur-lg fixed flex items-center top-0 left-0 w-full z-10 transition-transform duration-300 ${
+// 				isVisible ? "translate-y-0" : "-translate-y-full"
+// 			}`}
+// 		>
+// 			<nav className="flex justify-between items-center pt-4 pb-4 w-3/4 mx-auto">
+// 				<ul>
+// 					<li className="flex text-2xl font-bold text-black px-4 py-2">
+// 						Evoke.
+// 					</li>
+// 				</ul>
+
+// 				{/* hamburger */}
+// 				<ul>
+// 					<li
+// 						className="hover:cursor-pointer lg:hidden flex relative"
+// 						onClick={() => setView(!view)}
+// 					>
+// 						<svg
+// 							xmlns="http://www.w3.org/2000/svg"
+// 							fill="none"
+// 							viewBox="0 0 24 24"
+// 							strokeWidth={1.5}
+// 							stroke="currentColor"
+// 							className={`w-6 h-6 transition-transform duration-300 ${view ? "rotate-90" : "rotate-0"}`}
+// 						>
+// 							<path
+// 								strokeLinecap="round"
+// 								strokeLinejoin="round"
+// 								d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+// 							/>
+// 						</svg>
+// 					</li>
+// 				</ul>
+
+// 				{/* pc screen menu */}
+// 				<ul className="lg:flex lg:justify-center lg:items-center space-x-6 hidden">
+// 					{["/", "/blog", "/pov", "/gallery", "/aboutme"].map((href, index) => (
+// 						<Link
+// 							key={index}
+// 							href={href}
+// 							className="rounded-full px-4 py-2 text-gray-400 hover:text-black transition-colors duration-75 ease-in-out font-semibold hover:cursor-pointer underline-animation"
+// 						>
+// 							{["Home", "Blogs", "POV", "Gallery", "About Me"][index]}
+// 						</Link>
+// 					))}
+// 				</ul>
+// 			</nav>
+
+// 			{/* mobile and tab view menu */}
+// 			{view && (
+// 				<div
+// 					data-aos="fade-down" // AOS attribute for animation
+// 					data-aos-duration="500" // Animation duration
+// 					className="flex flex-col absolute top-20 right-0 w-full p-4 z-10 bg-slate-50 lg:hidden"
+// 				>
+// 					<ul className="flex flex-col items-center space-y-2 w-3/4 sm:w-1/4 mx-auto text-center">
+// 						{["/", "/blog", "/pov", "/gallery", "/aboutme"].map((href, index) => (
+// 							<Link
+// 								key={index}
+// 								href={href}
+// 								className="rounded-lg bg-black w-full py-2 text-white/90"
+// 							>
+// 								{["Home", "Blogs", "POV", "Gallery", "About Me"][index]}
+// 							</Link>
+// 						))}
+// 					</ul>
+// 				</div>
+// 			)}
+// 		</header>
+// 	);
+// };
+
+// export default Navbar;
