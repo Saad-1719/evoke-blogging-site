ba9496bbc6040efc306cd82dfcd23cb1619c316c
saad-1719 <chsaad1407@gmail.com>
2024-09-27 17:55:32 +0500

    Refactor getBlogs route to simplify code and improve error handling

--- app/api/getBlogs/route.ts
+++ app/api/getBlogs/route.ts
@@ -1,11 +1,10 @@
 import db from '../../../lib/db';
-import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
     try {
-      const [rows] = await db.query('SELECT * FROM blog');  // Adjust query based on your table structure
-      return NextResponse.json(rows);  // Return the data in JSON format
+      const [rows] = await db.query('SELECT * FROM blog'); 
+      return NextResponse.json(rows);
     } catch (error) {
       console.error('Failed to fetch blogs:', error);
       return NextResponse.json({ error: 'Failed to fetch blogs' }, { status: 500 });
