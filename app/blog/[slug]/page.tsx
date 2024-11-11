180eb03b1b7747f933b767d8c8d2f79673130ac3
saad-1719 <chsaad1407@gmail.com>
2024-11-11 18:47:01 +0500

    Refactor blog/[slug]/page.tsx to update fetching logic with no-store cache policy

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -20,9 +20,9 @@ const BlogPost = async ({ params }: Params) => {
 	const { slug } = params;
 
 	// Fetch all blog posts from the API
-	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getBlogs`
-	);
+	const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getBlogs`, {
+        cache: 'no-store',
+      });
 
 	if (!res.ok) {
 		// Handle the error if the blog posts cannot be fetched
