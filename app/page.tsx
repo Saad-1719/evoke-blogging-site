3b030b8063a4a574ee7fe0068ccb57cacc94665a
saad-1719 <chsaad1407@gmail.com>
2024-12-16 23:28:31 +0500

    Refactor Home component to update fetching logic with no-store cache policy

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,13 @@ interface Blog {
 export default async function Home() {
   // Fetch data directly within the component
   let blogs: Blog[] = [];
-  
+
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getBlogs`); // Optionally set cache behavior
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getBlogs`, {
+      // Ensure that this request is never cached
+      cache: "no-store", // Prevent caching of this request
+    });
+
     if (!res.ok) {
       console.error('Failed to fetch blogs:', res.status, res.statusText);
       return (
@@ -26,9 +30,9 @@ export default async function Home() {
         </main>
       );
     }
-    
+
     blogs = await res.json();
-    
+
     // Sort blogs by dateAdded in descending order
     blogs.sort((a, b) => new Date(b.dateAdded).getTime() - new Date(a.dateAdded).getTime());
   } catch (error) {
